cb66d6575b03a4ccfd8247c933b402a9cd33650a
felix <nibelungen@gmx.ch>
2017-03-26 23:13:12 +0200

    Add ReferenceVolumeSlicer plugin
    Small plugin that allows to conveniently navigate xy, xy and yz sections
    and to save them.

---
+++ src/main/java/ReferenceVolumneSlicer.java
@@ -0,0 +1,63 @@
+import gui.VolumeSlicerDialog;
+import ij.ImagePlus;
+import ij.WindowManager;
+import ij.io.OpenDialog;
+import io.scif.services.DatasetIOService;
+import net.imagej.Dataset;
+import net.imagej.ImageJ;
+import net.imagej.legacy.LegacyService;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Felix Meyenhofer
+ */
+@SuppressWarnings("WeakerAccess")
+@Plugin(type = Command.class, menuPath = "Plugins > Allen Brain Atlas > Reference Volume Slicer")
+public class ReferenceVolumneSlicer implements Command {
+
+    @Parameter
+    DatasetIOService ioService;
+
+    @Parameter
+    LegacyService legacyService;
+
+
+    @Override
+    public void run() {
+        /* Reference to the active image(-display) */
+        ImagePlus imp = WindowManager.getCurrentImage();
+
+        if (imp == null) {
+            OpenDialog dialog = new OpenDialog("Choose an image file (monochrome volume (xyz)");
+            try {
+                Dataset dataset = ioService.open(dialog.getPath());
+                imp = legacyService.getImageMap().registerDataset(dataset);
+                imp.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        VolumeSlicerDialog.createAndShow(imp);
+    }
+
+
+    public static void main(final String... args) throws Exception {
+        // Get the ImageJ instance
+        final ImageJ ij = net.imagej.Main.launch(args);
+
+        // Open an image
+//        File file = new File ("/Users/turf/switchdrive/SJMCS_Thesis/data/aba/mouse-ccf-3/reference-volumes/average_template_50.nrrd");
+        File file = new File("/Users/turf/Desktop/average_template_25.tif");
+        Dataset dataset = ij.scifio().datasetIO().open(file.getAbsolutePath());
+        ij.display().createDisplay(dataset);
+
+        // Run the plugin
+        ij.command().run(ReferenceVolumneSlicer.class, true);
+    }
+}
