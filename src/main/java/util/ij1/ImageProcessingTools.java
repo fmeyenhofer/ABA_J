e4b8cec97065a37ebabf0664e462d231ad9dbbf9
felix <nibelungen@gmx.ch>
2017-09-21 22:47:18 +0200

    Add some ij1 image processing utility functions

---
+++ src/main/java/util/ij1/ImageProcessingTools.java
@@ -0,0 +1,56 @@
+package util.ij1;
+
+import ij.ImagePlus;
+import ij.process.ImageProcessor;
+import net.imagej.ImageJ;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+
+import java.io.IOException;
+
+/**
+ * @author Felix Meyenhofer
+ */
+public class ImageProcessingTools {
+    
+
+    public static void adjustContrast(ImageProcessor processor, double saturationFraction) {
+
+        if (0.0 > saturationFraction || saturationFraction > 1.0) {
+            throw new RuntimeException("The saturation fraction has to be in the interval [0...1].");
+        }
+
+        int maxPixel = (int) (processor.getPixelCount() * saturationFraction);
+        int[] hist = processor.getHistogram();
+
+        int lowerBound = 0;
+        while (hist[lowerBound] == 0) {
+            lowerBound++;
+        }
+
+        int upperBound = 0;
+        int nPixel = 0;
+        while (nPixel < maxPixel) {
+            nPixel += hist[upperBound++];
+        }
+
+//        System.out.println("min = " + lowerBound + ", max = " + upperBound);
+
+        processor.setMinAndMax((double) lowerBound, (double) upperBound);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        final ImageJ ij = new net.imagej.ImageJ();
+        ij.ui().showUI();
+
+        Object img = ij.io().open("/Users/turf/switchdrive/SJMCS/data/devel/section2volume/average_template_25um_coronal_295-315.tif");
+
+        ImagePlus imp = ImageJFunctions.wrap((RandomAccessibleInterval) img, "test");
+        imp.show();
+
+        ImageProcessingTools.adjustContrast(imp.getProcessor(), 0.99);
+
+        imp.updateAndRepaintWindow();
+    }
+}
