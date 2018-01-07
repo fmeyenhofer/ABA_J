6636ee18ce6e1cc110b6b8e19ac7d9eb6aa9ff21
felix <nibelungen@gmx.ch>
2018-01-07 21:13:34 +0100

    Tweak generics for SectionMask plugin

--- src/main/java/SectionMask.java
+++ src/main/java/SectionMask.java
@@ -1,8 +1,9 @@
 import net.imagej.Dataset;
 import net.imagej.ImageJ;
 import net.imagej.ops.OpService;
-import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
 import net.imglib2.type.logic.BitType;
+import net.imglib2.type.numeric.real.DoubleType;
 import org.scijava.ItemIO;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
@@ -10,31 +11,33 @@ import org.scijava.plugin.Plugin;
 import org.scijava.ui.UIService;
 import img.SectionImageTool;
 
+import java.io.IOException;
+
 /**
  * @author Felix Meyenhofer
  */
+@SuppressWarnings("FieldCanBeLocal")
 @Plugin(type = Command.class, menuPath = "Plugins > Allen Brain Atlas > Pre-Processing > Section Mask")
 public class SectionMask implements Command{
 
     @Parameter
-    OpService ops;
+    private OpService ops;
 
     @Parameter
-    UIService ui;
-
+    private UIService ui;
+                                                                                         
 
     @Parameter(type = ItemIO.INPUT)
-    Dataset dataset;
+    private Dataset dataset;
 
     @Parameter(label = "Gaussian smoothing (-1 -> auto estimate)")
-    Double sigma = -1.;
+    private Double sigma = -1.;
 
 
     @Override
     public void run() {
-
-        RandomAccessibleInterval rai = dataset.getImgPlus().getImg();
-        RandomAccessibleInterval<BitType> msk;
+        Img<DoubleType> rai = (Img) dataset.getImgPlus();
+        Img<BitType> msk;
 
         if (sigma.equals(-1.)) {
             msk = SectionImageTool.createMask(rai, ops);
@@ -46,10 +49,11 @@ public class SectionMask implements Command{
     }
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ImageJ ij = new ImageJ();
         ij.ui().showUI();
-
+        Object obj = ij.io().open("/Users/turf/switchdrive/SJMCS/data/devel/small-deformations/crym(cy3)_gng2(A488)_IHC(150914)_DGC4_1 - 2016-01-28 05.03.56-FITC_ROI-00.tif");
+        ij.ui().show(obj);
         ij.command().run(SectionMask.class, true);
     }
 }
